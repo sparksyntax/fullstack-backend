17246bb633fb2b9c6ca8450ad1cad91768daca42
Priyadharshan <dharshanofflac24@gmail.com>
2025-06-21 22:11:40 +0530

    Initial commit

---
+++ fullstack-backend/src/main/java/com/example/fullstack_backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.fullstack_backend.repository;
+
+import com.example.fullstack_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
